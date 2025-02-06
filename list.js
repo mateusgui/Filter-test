9f1b3cb820c14a87f7c4ed8eb9fc9761cb344987
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-06 14:09:44 -0300

    html pronto

--- list.js
+++ list.js
@@ -1,15 +1,24 @@
 const items = [
-
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
-
-    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
-
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
-
-    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
-
-    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
-
-    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-
+    { name: 'Ahri', src: 'img/ahri.png', lane: 'Mid' },
+    { name: 'Alistar', src: 'img/alistar.png', lane: 'Suporte' },
+    { name: 'Azir', src: 'img/azir.png', lane: 'Mid' },
+    { name: 'Brand', src: 'img/brand.png', lane: 'Mid Suporte' },
+    { name: 'Corki', src: 'img/corki.png', lane: 'Mid ADC' },
+    { name: 'Darius', src: 'img/darius.png', lane: 'Top' },
+    { name: 'Draven', src: 'img/draven.png', lane: 'ADC' },
+    { name: 'Garen', src: 'img/garen.png', lane: 'Top' },
+    { name: 'Lee Sin', src: 'img/leesin.png', lane: 'Jungle' },
+    { name: 'Lulu', src: 'img/lulu.png', lane: 'Suporte' },
+    { name: 'Nasus', src: 'img/nasus.png', lane: 'Top' },
+    { name: 'Shyvana', src: 'img/shyvana.png', lane: 'Top Jungle' },
+    { name: 'Smolder', src: 'img/smolder.png', lane: 'Top Mid ADC' },
+    { name: 'Sona', src: 'img/sona.png', lane: 'Suporte' },
+    { name: 'Teemo', src: 'img/teemo.png', lane: 'Top Mid' },
+    { name: 'Thresh', src: 'img/thresh.png', lane: 'Suporte' },
+    { name: 'Tristana', src: 'img/tristana.png', lane: 'Mid ADC' },
+    { name: 'Vayne', src: 'img/vayne.png', lane: 'ADC' },
+    { name: 'Warwick', src: 'img/warwick.png', lane: 'Jungle' },
+    { name: 'Yasuo', src: 'img/yasuo.png', lane: 'Top Mid' },
+    { name: 'Zac', src: 'img/zac.png', lane: 'Jungle' },
+    { name: 'Zed', src: 'img/zed.png', lane: 'Top Mid' }
 ]
